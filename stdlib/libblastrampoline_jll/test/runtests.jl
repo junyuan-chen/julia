d40bd79b3aa7caaaa1cd1c25886589ae91084b30
Viral B. Shah <viral@mayin.org>
2021-02-17 16:42:57 -0500

    Add .jl extension to libblastrampoline_jll Add a test

--- stdlib/libblastrampoline_jll/test/runtests.jl
+++ stdlib/libblastrampoline_jll/test/runtests.jl
@@ -1,7 +1,7 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
-using Test, libblastrampoline_jll
+using Test, Libdl, libblastrampoline_jll
 
 @testset "libblastrampoline_jll" begin
-    true
+    @test isa(Libdl.dlsym(Libdl.dlopen(:libblastrampoline), :dgemm_64_), Ptr{Nothing})
 end
